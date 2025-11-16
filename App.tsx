fbc6f52dadbabb88821b09f5e928f047e3cf4efe
Rodolfo <j10rodolfo@gmail.com>
2025-11-16 16:56:00 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,22 @@
+
+
+import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import {
+  SafeAreaProvider,
+} from 'react-native-safe-area-context';
+import { Home } from './src/screen/Home/Home';
+
+function App() {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <SafeAreaProvider>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <Home/>
+    </SafeAreaProvider>
+  );
+}
+
+
+
+export default App;
---
+++ src/screen/Home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import { styles } from './HomeStyles';
+
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+       <Text>Home Screen</Text>
+    </View>
+  );
+}
